1571f0d29dfb7daf9f8aa25a1bf928f37454c5ff
20gns <b_h14@naver.ccom>
2023-11-13 23:05:38 +0900

    .

--- c_23_09_12/c.c
+++ c_23_09_12/c.c
@@ -7,23 +7,30 @@
 #define INF 1000L
 
 typedef struct GraphType {
-	int n;	// 정점의 개수
+	/**정점의 개수*/
+	int n;
+	/**맵*/
 	int weight[MAX_VERTICES][MAX_VERTICES];
 } GraphType;
 
+
+/**추가되면 값이 변경*/
 int selected[MAX_VERTICES];
+
+/**거리*/
 int distance[MAX_VERTICES];
 
+/**노드의 수만큼 */
 int get_min_vertex(int n)
 {
 	int v, i;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)		//selected가 false인 경우 i값을 저장
 		if (!selected[i]) {
 			v = i;
 			break;
 		}
 	for (i = 0; i < n; i++)
-		if (!selected[i] && (distance[i] < distance[v])) v = i;
+		if (!selected[i] && (distance[i] < distance[v])) v = i;		// 선택되지 않았으며 v보다 더 적은 크기의 거리가 있을때 v에 저장
 	return (v);
 }
 void prim(GraphType* g, int s)
@@ -31,24 +38,25 @@ void prim(GraphType* g, int s)
 	int i, u, v;
 
 	for (u = 0; u < g->n; u++)
-		distance[u] = INF;
-	distance[s] = 0;
-	for (i = 0; i < g->n; i++) {
+		distance[u] = INF;				// distance를 INF로 초기화
+	distance[s] = 0;					//s는 시작점. 본인 위치이기 때문에 0
+	for (i = 0; i < g->n; i++) {		
 		u = get_min_vertex(g->n);
 		selected[u] = TRUE;
-		if (distance[u] == INF) return;
-		printf("정점 %d 추가\n", u);
+		if (distance[u] == INF) return;	//갈 수 없으면 return
+		printf("정점 %d 추가\n", u+1);
 		for (v = 0; v < g->n; v++)
-			if (g->weight[u][v] != INF)
-				if (!selected[v] && g->weight[u][v] < distance[v])
+			if (g->weight[u][v] != INF) //해당 행에 갈 수 있는 열이 있다면
+				if (!selected[v] && g->weight[u][v] < distance[v]) // 선택되지 않았고 가장 작다면
 					distance[v] = g->weight[u][v];
 	}
 }
 
 int main(void)
 {
-	GraphType g = { 10,
-	{{ 0, 3, INF, INF, INF, 11, 12, INF, INF, INF },
+	GraphType g = { 10/*n을 초기화 하는 부분*/,
+	{
+	{ 0, 3, INF, INF, INF, 11, 12, INF, INF, INF },
 	{ 3,  0, 5, 4, 1, 7, 8 , INF, INF, INF },
 	{ INF, 5, 0, 2, INF, INF, 6, 5, INF, INF },
 	{ INF, 4, 2, 0, 13, INF, INF, 14, INF, 16 },
@@ -59,7 +67,7 @@ int main(void)
 	{ INF, INF, INF, INF, 18, INF, INF, INF, 0, 10 },
 	{ INF, INF, INF, 16, INF, INF, INF, 15, 10, 0 }
 	}};
-	prim(&g, 1);
+	prim(&g, 0);
 	return 0;
 
 }
