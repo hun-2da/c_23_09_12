14c996ec6eab61387bec2c64eb03eea262ab05d8
20gns <b_h14@naver.ccom>
2023-11-28 10:46:32 +0900

    123

--- c_23_09_12/c.c
+++ c_23_09_12/c.c
@@ -1,82 +1,84 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+
 #define MAX_SIZE 20
+int sorted[MAX_SIZE]; // 추가 공간이 필요
+// i는 정렬된 왼쪽리스트에 대한 인덱스
+// j는 정렬된 오른쪽리스트에 대한 인덱스
+// k는 정렬될 리스트에 대한 인덱스
+int m = 0, c = 0;
+int j = 0;
 
-int msum = 0;
-int qsum = 0;
-int sum[] = { 0,0 };
 
-/**쉘 정렬에서 수행될 insert sort*/
-void inc_insertion_sort(int list[], int first, int last, int gap)
-// list는 random하게 작성됨,
-// first는 0~gap 
-// last는 최대치
-// gap 은 점프 거리
+void merge(int list[], int left, int mid, int right)
 {
-	/**key : 비교할 대상*/
-	int i, j, key;
-	for (i = first + gap; i <= last; i = i + gap) {	
-		//띄어 넘어 가면서 비교하기 위한 for문, 
-		// first + gap : 비교 대상
-		key = list[i];
-		qsum++;
-		for (j = i - gap; j >= first && key < list[j]; j = j - gap) {
-			// i : 비교할 대상의 위치 즉 j는 구분할 값
-			// j가 first보다 크면서 && 비교 대상보다 클때
-			// j는 - gap
-			list[j + gap] = list[j];
-			msum++;
-			
-		}
-		list[j + gap] = key;
+	int i, j, k, l;
+	i = left; j = mid + 1; k = left;
+	// 분할 정렬된 list의 합병
+	while (i <= mid && j <= right) {
+		c++;
+		if (list[i] <= list[j]) sorted[k++] = list[i++];
+		else sorted[k++] = list[j++];
 	}
-}
-/**쉘 정렬*/
-void shell_sort(int list[], int n) {
-	int i, gap;
-	if (sum[0] == 0) 
-		printf("\n\nShell sort\n\n");
-	for (gap = n / 2; gap > 0; gap = gap / 2) {		//gap을 총 개수의 반으로 지정
-		if ((gap % 2) == 0) gap++;					//gap이 짝수이면 1증가? 
-		for (i = 0; i < gap; i++) {
-			inc_insertion_sort(list, i, n - 1, gap);
-		}
-		if (sum[0] == 0) {
-		
-			for (int m = 0; m < MAX_SIZE; m++)
-				printf(" %d ", list[m]);
-			printf("\n");
-		}
+	if (i > mid) 	// 남아 있는 레코드의 일괄 복사
+		for (l = j; l <= right; l++)
+			sorted[k++] = list[l];
+	else 	// 남아 있는 레코드의 일괄 복사
+		for (l = i; l <= mid; l++)
+			sorted[k++] = list[l];
+	// 배열 sorted[]의 리스트를 배열 list[]로 복사
+	for (l = left; l <= right; l++) {
+		list[l] = sorted[l];
+		m++;
 	}
-}
+	if (j == 0) {
+		for (int i = 0; i < MAX_SIZE; i++)
+			printf(" %d ", list[i]);
+		printf("\n");
+	}
 
+}
+void merge_sort(int list[], int left, int right)
+{
+	int mid;
+	if (left < right)
+	{
+		mid = (left + right) / 2;              // 리스트의 균등분할
+		merge_sort(list, left, mid);     // 부분리스트 정렬
+		merge_sort(list, mid + 1, right);//부분리스트 정렬
+		merge(list, left, mid, right);    // 합병
+	}
+}
 
 int main() {
 	int i;
 	int n = MAX_SIZE;
 	int list[20];
-	for (int j = 0; j < n; j++) {
+	int sum[2];
+	int big=0, small=0;
+
+	//for (j = 0; j < n; j++) {
 		srand(time(NULL));
-		for (i = 0; i < n; i++)      	// 난수 생성 및 출력 
+		for (i = 0; i < n; i++) {      	// 난수 생성 및 출력 
 			list[i] = rand() % 100; // 난수 발생 범위 0~99
 
+		}
 		if (j == 0) {
 			printf("Original List \n\n");
-			for (i = 0; i < n; i++)
-				printf(" %d ", list[i]);
+			printf(" %d ", list[i]);
 			printf("\n");
 		}
 
-		shell_sort(list, n);
-		sum[0] += msum;
-		sum[1] += qsum;
-		msum = qsum = 0;
-	}
+		merge_sort(list, 0,19);
+		sum[0] += m;
+		sum[1] += c;
+		big = small = m = c = 0;
+	//}
 
 	// 각 정렬들의 출력을 한번씩만 하기 위한 코드
-	printf("쉘 정렬의 이동 평균은 %d \n", sum[0]/n);
-	printf("쉘 정렬의 비교 평균은 %d \n", sum[1]/n);
+	printf("merge 정렬의 이동 평균은 %d \n", sum[0]/n);
+	printf("merge 정렬의 비교 평균은 %d \n", sum[1]/n);
 
 
 	return 0;
